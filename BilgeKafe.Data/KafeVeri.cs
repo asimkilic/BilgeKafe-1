c5aa06e6c43ae86dbf2cd18e31fe2f8ed2cab733
nazim yavuz <nazimkurtulusyavuz@gmail.com>
2021-09-11 21:36:33 +0300

    Aralara bıoşluk konuldu.

--- BilgeKafe.Data/KafeVeri.cs
+++ BilgeKafe.Data/KafeVeri.cs
@@ -9,8 +9,12 @@ namespace BilgeKafe.Data
     public class KafeVeri
     {
         public int MasaAdet { get; set; } = 20; // default olarak giriyoruz
+
         public List<Urun> Urunler { get; set; } = new List<Urun>();
+
         public List<Siparis> AktifSiparisler { get; set; } = new List<Siparis>();
+
         public List<Siparis> GecmisSiparisler { get; set; } = new List<Siparis>();
+
     }
 }
